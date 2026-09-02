4240cbe9651de7d5017f5e6c5e62c5d01db63e83
Ali Hassan <alohussan1@gmail.com>
2026-09-02 20:11:30 +0500

    Learning React Routing

---
+++ React-router-dom/src/App.jsx
@@ -0,0 +1,20 @@
+import {Routes, Route} from 'react-router-dom'
+import Home from './Pages/Home'
+import About from './Pages/About'
+import Contact from './Pages/Contact'
+import Navbar from './components/navbar'
+
+const App = () => {
+  return (
+    <div>
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={ <Home/>}/>
+        <Route path="/about" element={ <About/>}/>
+        <Route path="/contact" element={ <Contact/>}/>
+      </Routes>
+    </div>
+  )
+}
+
+export default App
